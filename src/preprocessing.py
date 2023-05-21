7fb467d9425d0e51af69bbac56310fcb7ea84d9b
mlopez0 <m.lopez@innopolis.university>
2023-05-21 14:50:07 -0300

    2. PreProcessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -40,21 +40,62 @@ def preprocess_data(
     working_val_df["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace=True)
     working_test_df["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace=True)
 
-    # 2. TODO Encode string categorical features (dytpe `object`):
-    #     - If the feature has 2 categories encode using binary encoding,
-    #       please use `sklearn.preprocessing.OrdinalEncoder()`. Only 4 columns
-    #       from the dataset should have 2 categories.
-    #     - If it has more than 2 categories, use one-hot encoding, please use
-    #       `sklearn.preprocessing.OneHotEncoder()`. 12 columns
-    #       from the dataset should have more than 2 categories.
-    # Take into account that:
-    #   - You must apply this to the 3 DataFrames (working_train_df, working_val_df,
-    #     working_test_df).
-    #   - In order to prevent overfitting and avoid Data Leakage you must use only
-    #     working_train_df DataFrame to fit the OrdinalEncoder and
-    #     OneHotEncoder classes, then use the fitted models to transform all the
-    #     datasets.
+   # 2. Encode string categorical features (dtype `object`):
+    obj_cols = list(working_train_df.select_dtypes("object").columns)
+    low_card_cols = [
+        col
+        for col in working_train_df[obj_cols].columns
+        if working_train_df[col].nunique() <= 2
+    ]
+    
+    high_card_cols = [
+        col
+        for col in working_train_df[obj_cols].columns
+        if working_train_df[col].nunique() > 2
+    ]
 
+    # Train the encoders
+    ohe = OneHotEncoder(handle_unknown="ignore",   sparse_output=False)
+    oe = OrdinalEncoder(handle_unknown="error")
+
+    ohe.fit(working_test_df[high_card_cols])
+    oe.fit(working_test_df[low_card_cols])
+
+    # Apply the encoders
+    dfs = {
+        x: y 
+        for x, y in enumerate([working_train_df, working_val_df, working_test_df])
+    }
+
+    dfs_hc_econded = {}
+    dfs_lc_encoded = {}
+
+    for index, value in dfs.items():
+        dfs_hc_econded[index] = pd.DataFrame(
+            ohe.transform(value[high_card_cols])
+        )
+        dfs_hc_econded[index].index = value[high_card_cols].index
+
+        dfs_lc_encoded[index] = pd.DataFrame(
+            oe.transform(value[low_card_cols])
+        )
+
+        dfs_lc_encoded[index].index = value[low_card_cols].index
+
+        init_col = dfs_hc_econded[index].shape[1]
+
+        fin_col = dfs_hc_econded[index].shape[1] + dfs_lc_encoded[index].shape[1]
+        dfs_lc_encoded[index].columns = [
+            col_n for col_n in range(init_col, fin_col)
+        ]
+
+        dfs[index] = (
+            value.drop(columns=[*high_card_cols, *low_card_cols])
+            .join(dfs_hc_econded[index])
+            .join(dfs_lc_encoded[index])
+        )
+
+        dfs[index].columns = dfs[index].columns.astype(str)
 
     # 3. TODO Impute values for all columns with missing data or, just all the columns.
     # Use median as imputing value. Please use sklearn.impute.SimpleImputer().
@@ -65,6 +106,11 @@ def preprocess_data(
     #     working_train_df DataFrame to fit the SimpleImputer and then use the fitted
     #     model to transform all the datasets.
 
+    imputer = SimpleImputer(strategy="median")
+    imputer.fit(dfs[0])
+
+    for index, value in dfs.items():
+        dfs[index] = imputer.transform(value)
 
     # 4. TODO Feature scaling with Min-Max scaler. Apply this to all the columns.
     # Please use sklearn.preprocessing.MinMaxScaler().
@@ -75,5 +121,10 @@ def preprocess_data(
     #     working_train_df DataFrame to fit the MinMaxScaler and then use the fitted
     #     model to transform all the datasets.
 
+    minmax = MinMaxScaler()
+    minmax.fit(dfs[0])
 
-    return None
+    for index, value in dfs.items():
+        dfs[index] = minmax.transform(value)
+
+    return dfs[0], dfs[1], dfs[2]
